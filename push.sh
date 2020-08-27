eda9a4cdaf49b2816c97a08eb86a1afb7340521d
亚黎 <958780308@qq.com>
2020-08-27 21:04:59 +0800

    deploy

--- push.sh
+++ push.sh
@@ -3,7 +3,5 @@ set -e
 git init
 git add -A
 git commit -m 'deploy'
-git push -f "https://${access_token}@gitee.com/aclor/webproject.git" master
-git push -f "https://${access_token}@github.com/willasas/gitalkapp.git" master
-start "https://github.com/willasas/gitalkapp"
-exec /bin/bash
+git push -f https://@github.com/willasas/gitalkapp.git master
+cd -
